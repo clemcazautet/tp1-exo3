43cb621d5a9c5f5cfda975735c48f7d72a768895
clemcazautet <67945337+clemcazautet@users.noreply.github.com>
2020-07-21 20:04:30 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,16 +4,15 @@ let y1 = -1
 let y2 = -2
 basic.forever(function () {
     led.plotBrightness(x, y, 255)
-    basic.pause(500)
     led.unplot(x, y)
     y += 1
     led.plot(x, y)
-    led.plotBrightness(x, y1, 100)
+    led.plotBrightness(x, y1, 140)
     led.unplot(x, y1)
     y1 += 1
-    led.plotBrightness(x, y1, 100)
-    led.plotBrightness(x, y2, 10)
+    led.plotBrightness(x, y1, 140)
+    led.plotBrightness(x, y2, 40)
     led.unplot(x, y2)
     y2 += 1
-    led.plotBrightness(x, y2, 10)
+    led.plotBrightness(x, y2, 40)
 })
