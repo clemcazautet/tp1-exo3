58a8ed07ab9e4d9c8cf302301dd45d38c0a3f1e8
clemcazautet <67945337+clemcazautet@users.noreply.github.com>
2020-07-23 23:51:27 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,20 @@
 let y = 0
-let x = randint(0, 4)
-let y1 = -1
-let y2 = -2
+let x = 0
+let y1 = 0
+let y2 = 0
 basic.forever(function () {
-    led.plotBrightness(x, y, 255)
+    y = 0
+    x = randint(0, 4)
+    while (y <= 6) {
+        y1 = y - 1
+        y2 = y1 - 1
+        led.plotBrightness(x, y1, 150)
+        led.plotBrightness(x, y2, 50)
+        led.plotBrightness(x, y, 255)
+        y += 1
+        basic.pause(200)
+        led.unplot(x, y2)
+    }
     led.unplot(x, y)
-    y += 1
-    led.plot(x, y)
-    led.plotBrightness(x, y1, 140)
     led.unplot(x, y1)
-    y1 += 1
-    led.plotBrightness(x, y1, 140)
-    led.plotBrightness(x, y2, 40)
-    led.unplot(x, y2)
-    y2 += 1
-    led.plotBrightness(x, y2, 40)
 })
